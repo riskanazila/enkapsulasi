e26bbe79d06414c6506c05dfff9f7a4fd4630936
WINDOWS 10 <WINDOWS 10@192.168.56.1>
2018-08-20 13:35:47 +0700

    jobsheet 1 selesai

---
+++ src/Latihan/FungsiPengembalianNilai.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class FungsiPengembalianNilai {
+    public static void main(String[] args) {
+        Scanner masukan = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("Program menjumlahkan 2 nilai");
+        System.out.print("Masukkan nlai a: ");
+        a = masukan.nextInt();
+        System.out.print("Masukkan nlai b: ");
+        b = masukan.nextInt();
+        System.out.print("Masukkan nlai c: ");
+        c = masukan.nextInt();
+        System.out.println();
+        System.out.println("Jumlah a + b = "+(jumlah(a,b)));
+        System.out.println("Jumlah b + c = "+(jumlah(b,c)));
+        System.out.println("Jumlah a + c = "+(jumlah(a,c)));
+    }
+    //konstruktor
+    private static int jumlah(int a, int b){
+        int hasil=a+b;
+        return hasil;
+    }
+}
